54988aafdbaa6529df2ba3e307f575f620eaccb7
KrugarValdes <sm.vit2003@gmail.com>
2025-06-08 17:37:49 +0600

    [~] Fix handle fast-scroll snapping glitch in WheelTimePicker

--- composeApp/src/commonMain/kotlin/band/effective/office/elevator/components/TimePickerModal.kt
+++ composeApp/src/commonMain/kotlin/band/effective/office/elevator/components/TimePickerModal.kt
@@ -14,6 +14,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -36,7 +40,7 @@ fun TimePickerModal(
     onClickOk: (LocalTime) -> Unit
 ) {
     val selectorSettings = WheelPickerDefaults.selectorProperties(enabled = false)
-    var time = startTime
+    var time by remember { mutableStateOf(startTime) }
 
     Column(
         modifier = modifier
