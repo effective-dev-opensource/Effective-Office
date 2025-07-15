e85fafffd611c27a2281e9878868366fd7d1ad3c
KrugarValdes <sm.vit2003@gmail.com>
2025-07-15 21:49:01 +0600

    feat: implement nested slot deletion functionality

--- clients/tablet/feature/slot/src/commonMain/kotlin/band/effective/office/tablet/feature/slot/presentation/SlotIntent.kt
+++ clients/tablet/feature/slot/src/commonMain/kotlin/band/effective/office/tablet/feature/slot/presentation/SlotIntent.kt
@@ -4,7 +4,8 @@ import band.effective.office.tablet.core.domain.model.Slot
 import kotlinx.datetime.LocalDateTime
 
 sealed interface SlotIntent {
-    data class ClickOnSlot(val slot: SlotUi) : SlotIntent
+    data class ClickToEdit(val slot: SlotUi) : SlotIntent
+    data class ClickToToggle(val slot: SlotUi.MultiSlot) : SlotIntent
     data class UpdateDate(val newDate: LocalDateTime) : SlotIntent
     data class UpdateRequest(val room: String) : SlotIntent
     data class Delete(val slot: Slot, val onDelete: () -> Unit) : SlotIntent
