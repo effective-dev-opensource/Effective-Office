5afec0b9ada1d76a1439a12e0867441d99148b55
KrugarValdes <sm.vit2003@gmail.com>
2025-08-18 15:16:58 +0600

    feat: Implement kiosk mode toggle on client

--- clients/tablet/composeApp/src/androidMain/kotlin/band/effective/office/tablet/ServerMessagingService.kt
+++ clients/tablet/composeApp/src/androidMain/kotlin/band/effective/office/tablet/ServerMessagingService.kt
@@ -1,18 +1,48 @@
 package band.effective.office.tablet
 
+import android.provider.Settings
 import android.util.Log
 import band.effective.office.tablet.core.data.api.Collector
+import band.effective.office.tablet.utils.KioskCommand
+import band.effective.office.tablet.utils.KioskCommandBus
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class ServerMessagingService() :
     FirebaseMessagingService(), KoinComponent {
     private val collector: Collector<String> by inject()
+    private val serviceScope = CoroutineScope(Dispatchers.Main)
+    private val kioskCommandBus = KioskCommandBus.getInstance()
 
     override fun onMessageReceived(message: RemoteMessage) {
-        Log.i("ReceivedMessage", message.data.toString())
-        collector.emit(message.from?.substringAfter("topics/")?.replace("-test", "") ?: "")
+        Log.i("FCM_MESSAGE", "From: ${message.from}, Data: ${message.data}")
+
+        if (message.data["type"] == "KIOSK_TOGGLE") {
+            handleKioskCommand(message)
+        } else {
+            val topic = message.from?.substringAfter("topics/")?.replace("-test", "") ?: ""
+            collector.emit(topic)
+        }
+    }
+
+    private fun handleKioskCommand(message: RemoteMessage) {
+        val deviceId = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
+        val targetDeviceId = message.data["deviceId"]
+        val enabled = message.data["enabled"]?.toBooleanStrictOrNull()
+
+        if (enabled == null) {
+            return
+        }
+
+        if (targetDeviceId == null || targetDeviceId == deviceId) {
+            val command = if (enabled) KioskCommand.Enable else KioskCommand.Disable
+            kioskCommandBus.sendCommand(command, serviceScope)
+        } else {
+            Log.d("KIOSK_COMMAND", "Command ignored - not for this device")
+        }
     }
 }
