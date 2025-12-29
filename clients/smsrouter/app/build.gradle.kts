cc907ec9a65054fc6a3cd046c455c367dd871497
KrugarValdes <sm.vit2003@gmail.com>
2025-12-29 14:30:40 +0600

    chore: update smsrouter version to 1.0.0

--- clients/smsrouter/app/build.gradle.kts
+++ clients/smsrouter/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "band.effective.office.smsrouter"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
