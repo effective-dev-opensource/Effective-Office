65be1f84ed6edb50937655bb3dbb0ba9b231715b
Max <kirova32a@mail.ru>
2026-02-26 13:05:14 +0600

    feat: add check web hook button

--- clients/smsrouter/app/build.gradle.kts
+++ clients/smsrouter/app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.lifecycle.viewmodel)
     implementation(libs.androidx.navigation.compose)
