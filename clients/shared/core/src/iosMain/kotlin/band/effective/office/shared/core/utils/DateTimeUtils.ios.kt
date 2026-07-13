4727661c751283bf95c7a493d426da8bd531b038
denwritescode <suprundenis@gmail.com>
2026-07-13 22:54:51 +0300

    + fixing compilation errors for tablet iOS target

--- clients/shared/core/src/iosMain/kotlin/band/effective/office/shared/core/utils/DateTimeUtils.ios.kt
+++ clients/shared/core/src/iosMain/kotlin/band/effective/office/shared/core/utils/DateTimeUtils.ios.kt
@@ -4,12 +4,10 @@ import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.toNSDateComponents
 import platform.Foundation.NSCalendar
 import platform.Foundation.NSDateFormatter
-import platform.Foundation.NSLocale
 
 actual fun LocalDateTime.toLocalisedString(pattern: String): String {
     val dateFormatter = NSDateFormatter()
     dateFormatter.dateFormat = pattern
-    dateFormatter.locale = NSLocale.currentLocale
 
     val calendar = NSCalendar.currentCalendar
     val dateComponents = toNSDateComponents()
--- clients/tablet/core/ui/src/iosMain/kotlin/band/effective/office/tablet/core/ui/utils/DateFormatter.ios.kt
+++ clients/tablet/core/ui/src/iosMain/kotlin/band/effective/office/tablet/core/ui/utils/DateFormatter.ios.kt
@@ -2,5 +2,6 @@ package band.effective.office.tablet.core.ui.utils
 
 import platform.Foundation.NSLocale
 import platform.Foundation.currentLocale
+import platform.Foundation.languageCode
 
-actual fun getCurrentLanguageCode(): String = NSLocale.currentLocale.languageCode
+actual fun getCurrentLanguageCode(): String = NSLocale.currentLocale.languageCode ?: "en"
